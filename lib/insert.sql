e0e32314431052584f8418455836c269d32a6fac
Winnie Mendoza <wrm956@gmail.com>
2022-08-15 11:20:59 -0400

    commit tree 35be3598fe3f177dd0de39267f1dd665e71ca74d

---
+++ lib/insert.sql
@@ -0,0 +1,24 @@
+INSERT INTO
+bears (name, age, sex, color, temperament, alive)
+VALUES ('Mr. Chocolate', 2, 'F', 'brown', 'sweet', 1);
+INSERT INTO
+bears (name, age, sex, color, temperament, alive)
+VALUES ('Rowdy', 56, 'M', 'white', 'hungry', 1);
+INSERT INTO
+bears (name, age, sex, color, temperament, alive)
+VALUES ('Tabitha', 5, 'F', 'magenta', 'moody', 1);
+INSERT INTO
+bears (name, age, sex, color, temperament, alive)
+VALUES ('Sergeant Brown', 8, 'F', 'pink', 'grumpy', 1);
+INSERT INTO
+bears (name, age, sex, color, temperament, alive)
+VALUES ('Melissa', 3, 'M', 'green', 'happy', 1);
+INSERT INTO
+bears (name, age, sex, color, temperament, alive)
+VALUES ('Grinch', 6, 'F', 'purple', 'excited', 1);
+INSERT INTO
+bears (name, age, sex, color, temperament, alive)
+VALUES ('Wendy', 9, 'M', 'yellow', 'thirsty', 1);
+INSERT INTO
+bears (name, age, sex, color, temperament, alive)
+VALUES (NULL, 1, 'F', 'black', 'euphoric', 1);
